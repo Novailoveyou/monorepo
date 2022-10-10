9180d1898ff3089323351a9761a3562679230588
novailoveyou <novailoveyoux@gmail.com>
2022-10-11 01:09:00 +0300

    informed a user that the work is ongoing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>
+  return <div>Hello World (Currently working on my portfolio)</div>
 }
 
 export default Home
