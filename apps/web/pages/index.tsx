1a85b6c1e7d867cf4347887d2caad02f22ef3f96
novailoveyou <novailoveyoux@gmail.com>
2023-02-15 17:34:08 +0300

    feat: added table generation

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,13 +1,69 @@
 // import { Button } from "ui";
+import { useEffect, useState } from 'react'
 import StyledJsxV1Button from 'styled-jsx/v1/Button'
 
+export const LENGTH = 10
+export const INITIAL_LENGTH = 0
+
 export default function Web() {
+  const [length, setLength] = useState(INITIAL_LENGTH)
+  const [width, setWidth] = useState(INITIAL_LENGTH)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerHeight)
+      setLength(window.innerWidth / LENGTH)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  console.log(length)
+  console.log(width)
   return (
     <div>
       <h1>Web</h1>
       <StyledJsxV1Button as='a' type='button'>
         123
       </StyledJsxV1Button>
+      <table>
+        <tbody>
+          {Array.from({ length }).map((_, trI) => (
+            <tr
+              key={trI}
+              style={{
+                fontSize: '12px',
+                maxWidth: `${LENGTH}px`,
+                minWidth: `${LENGTH}px`,
+                minHeight: `${LENGTH}px`,
+                maxHeight: `${LENGTH}px`,
+                border: 0,
+                overflow: 'hidden',
+                backgroundColor: trI % 2 === 0 ? '#333' : '#666',
+                color: '#fff'
+              }}>
+              {Array.from({ length }).map((_, tdI) => (
+                <td
+                  key={tdI}
+                  style={{
+                    fontSize: '12px',
+                    maxWidth: `${LENGTH}px`,
+                    minWidth: `${LENGTH}px`,
+                    minHeight: `${LENGTH}px`,
+                    maxHeight: `${LENGTH}px`,
+                    border: 0,
+                    overflow: 'hidden'
+                  }}>
+                  {trI}:{tdI}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
