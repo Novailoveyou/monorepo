60ab4d23f86c16eaa334a1b87471cdbeb433fa77
novailoveyou <novailoveyoux@gmail.com>
2023-02-19 09:55:40 +0300

    feat: temp testing UI

--- apps/novailoveyou-com/app/page.tsx
+++ apps/novailoveyou-com/app/page.tsx
@@ -7,48 +7,53 @@ import classNames from './Page.module.sass'
 const Home = () => {
   return (
     <main className={clsx(classNames.main)}>
-      <Div>
-        <Text
-          elementType='h1'
-          variant='nova'
-          fontWeight={700}
-          theme='light'
-          textTransform='uppercase'
-          className={clsx(classNames.h1)}>
-          <Text elementType='div' variant='nova' size='82' lineHeight='100'>
-            Hello!
-          </Text>{' '}
-          <Text
-            elementType='div'
-            variant='nova'
-            size='47'
-            fontWeight={400}
-            lineHeight='100'
-            textTransform='lowercase'>
-            My name is
-          </Text>{' '}
-          <Text elementType='div' variant='nova' size='51' lineHeight='100'>
-            Ilia Orlov
-          </Text>
-        </Text>
-        <Text
-          elementType='p'
-          variant='nova'
-          size='22'
-          fontWeight={500}
-          lineHeight='120'
-          theme='light'
-          className={clsx(classNames.p)}>
-          Full Stack Web Developer specializing in TypeScript
-        </Text>
-        <Button
-          elementType='button'
-          variant='nova'
-          size='lg'
-          theme='primary'
-          type='button'>
-          Hire me
-        </Button>
+      <Div className={clsx(classNames.container)}>
+        <Div className={clsx(classNames.plate)}>
+          <Div className={clsx(classNames.left)}>
+            <Text
+              elementType='h1'
+              variant='nova'
+              fontWeight={700}
+              theme='light'
+              textTransform='uppercase'
+              className={clsx(classNames.h1)}>
+              <Text elementType='div' variant='nova' size='82' lineHeight='100'>
+                Hello!
+              </Text>{' '}
+              <Text
+                elementType='div'
+                variant='nova'
+                size='47'
+                fontWeight={400}
+                lineHeight='100'
+                textTransform='lowercase'>
+                My name is
+              </Text>{' '}
+              <Text elementType='div' variant='nova' size='51' lineHeight='100'>
+                Ilia Orlov
+              </Text>
+            </Text>
+            <Text
+              elementType='p'
+              variant='nova'
+              size='22'
+              fontWeight={500}
+              lineHeight='120'
+              theme='light'
+              className={clsx(classNames.p)}>
+              Full Stack Web Developer specializing in TypeScript
+            </Text>
+            <Button
+              elementType='button'
+              variant='nova'
+              size='lg'
+              theme='primary'
+              className={clsx(classNames.button)}
+              type='button'>
+              Hire me
+            </Button>
+          </Div>
+        </Div>
       </Div>
     </main>
   )
